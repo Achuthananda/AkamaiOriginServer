eb0db7034d8267007ae0bc9fe1053e2330b7627e
Achuthananda M P <achuthadivine@gmail.com>
2022-07-11 08:04:54 +0530

    Restrutured the srcfiles

--- initialize.sh
+++ initialize.sh
@@ -13,14 +13,13 @@ server {
 EOF
 mv /etc/nginx/sites-enabled/flask_app /etc/nginx/sites-enabled/flask_app.default
 cp nginx.conf /etc/nginx/sites-enabled/flask_app
-sudo apt install certbot python-certbot-nginx
+sudo apt install --assume-yes certbot python-certbot-nginx
 sudo certbot --nginx --noninteractive --agree-tos -d $HOSTNAME --register-unsafely-without-email
 sudo apt install ufw
 sudo systemctl start ufw && sudo systemctl enable ufw
 sudo ufw allow http
 sudo ufw allow https
-sudo ufw enable
-cp -r !\(README.md|LICENSE\) /home/flask_app_project/flask_app/
-cd /home/flask_app_project/flask_app/
+echo "y" | sudo ufw enable
+cp -r srcfiles/* /home/flask_app_project/flask_app/
 ps aux | grep gunicorn | awk '{print $2}' | xargs kill -9
 sudo supervisorctl reload
